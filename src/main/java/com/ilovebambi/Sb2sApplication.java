1e177577501551cc64ace68ffce8ffec7514e5a3
killroad <killroad@gmail.com>
2018-03-04 23:11:54 +0900

    boot 2.0 GA버전 적용, devtools적용

--- src/main/java/com/ilovebambi/Sb2sApplication.java
+++ src/main/java/com/ilovebambi/Sb2sApplication.java
@@ -4,12 +4,12 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@EnableAutoConfiguration
+@SpringBootApplication
 public class Sb2sApplication implements CommandLineRunner {
 
-	private static final Logger LOGGER = LoggerFactory.getLogger(Sb2sApplication.class);
+	private static  final Logger LOGGER = LoggerFactory.getLogger(Sb2sApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(Sb2sApplication.class, args);
